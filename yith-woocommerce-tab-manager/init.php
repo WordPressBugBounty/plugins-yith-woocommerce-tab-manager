110d7e95b36411bc8fcb3f47f02b8ef73f25275d
Sean Murphy <sean@iamseanmurphy.com>
2025-06-09 12:20:41 +0000

    2.5.0

--- yith-woocommerce-tab-manager/init.php
+++ yith-woocommerce-tab-manager/init.php
@@ -3,18 +3,18 @@
  * Plugin Name: YITH WooCommerce Tab Manager
  * Plugin URI: https://yithemes.com/themes/plugins/yith-woocommerce-tab-manager/
  * Description: <code><strong>YITH WooCommerce Tab Manager</strong></code> allows you to add additional tabs in the product page. <a href ="https://yithemes.com">Get more plugins for your e-commerce shop on <strong>YITH</strong></a>
- * Version: 2.4.0
+ * Version: 2.5.0
  * Author: YITH
  * Author URI: https://yithemes.com/
  * Text Domain: yith-woocommerce-tab-manager
  * Domain Path: /languages/
- * WC requires at least: 9.6
- * WC tested up to: 9.8
+ * WC requires at least: 9.7
+ * WC tested up to: 9.9
  * Requires Plugins: woocommerce
  *
  * @author YITH <plugins@yithemes.com>
  * @package YITH WooCommerce Tab Manager
- * @version 2.4.0
+ * @version 2.5.0
  */
 
 /*
@@ -78,7 +78,7 @@ if ( ! function_exists( 'yith_plugin_registration_hook' ) ) {
 
 
 if ( ! defined( 'YWTM_VERSION' ) ) {
-	define( 'YWTM_VERSION', '2.4.0' );
+	define( 'YWTM_VERSION', '2.5.0' );
 }
 
 if ( ! defined( 'YWTM_FREE_INIT' ) ) {
--- yith-woocommerce-tab-manager/readme.txt
+++ yith-woocommerce-tab-manager/readme.txt
@@ -5,7 +5,7 @@ Tags: tab, woocommerce, product, custom tab, e-commerce, ecommerce, commerce, gl
 Requires PHP: 7.4
 Requires at least: 6.6
 Tested up to: 6.8
-Stable tag: 2.4.0
+Stable tag: 2.5.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -109,6 +109,10 @@ If none of the previous listed actions helps you solve the problem, then, submit
 You can report security bugs through the Patchstack Vulnerability Disclosure Program. The Patchstack team help validate, triage and handle any security vulnerabilities. [Report a security vulnerability.](https://patchstack.com/database/vdp/yith-woocommerce-tab-manager)
 
 == Changelog ==
+
+= 2.5.0 - Released on 09 June 2025 =
+* New: Support for WooCommerce 9.9
+* Update: YITH plugin framework
 
 = 2.4.0 - Released on 22 April 2025 =
 * New: Support for WordPress 6.8
