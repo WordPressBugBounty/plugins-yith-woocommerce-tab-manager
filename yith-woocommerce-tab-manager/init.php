19bad41c8fa20093c212175b06611ad99d9118e1
Sean Murphy <sean@iamseanmurphy.com>
2026-03-06 11:20:04 +0000

    2.12.0

--- yith-woocommerce-tab-manager/init.php
+++ yith-woocommerce-tab-manager/init.php
@@ -3,18 +3,18 @@
  * Plugin Name: YITH WooCommerce Tab Manager
  * Plugin URI: https://yithemes.com/themes/plugins/yith-woocommerce-tab-manager/
  * Description: <code><strong>YITH WooCommerce Tab Manager</strong></code> allows you to add additional tabs in the product page. <a href ="https://yithemes.com">Get more plugins for your e-commerce shop on <strong>YITH</strong></a>
- * Version: 2.11.0
+ * Version: 2.12.0
  * Author: YITH
  * Author URI: https://yithemes.com/
  * Text Domain: yith-woocommerce-tab-manager
  * Domain Path: /languages/
- * WC requires at least: 10.3
- * WC tested up to: 10.5
+ * WC requires at least: 10.4
+ * WC tested up to: 10.6
  * Requires Plugins: woocommerce
  *
  * @author YITH <plugins@yithemes.com>
  * @package YITH WooCommerce Tab Manager
- * @version 2.11.0
+ * @version 2.12.0
  */
 
 /*
@@ -78,7 +78,7 @@ if ( ! function_exists( 'yith_plugin_registration_hook' ) ) {
 
 
 if ( ! defined( 'YWTM_VERSION' ) ) {
-	define( 'YWTM_VERSION', '2.11.0' );
+	define( 'YWTM_VERSION', '2.12.0' );
 }
 
 if ( ! defined( 'YWTM_FREE_INIT' ) ) {
